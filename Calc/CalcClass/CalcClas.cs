3a649877a8d01ef7b14e162293c66d99bf279357
AndrewKalushka <andrew.kalushka@gmail.com>
2019-11-13 21:37:06 +0200

    a2

--- Calc/CalcClass/CalcClas.cs
+++ Calc/CalcClass/CalcClas.cs
@@ -8,5 +8,80 @@ namespace CalcClass
 {
     public class CalcClas
     {
+        /// <summary> 
+        /// Функція складання числа а і b 
+        /// </summary> 
+        /// <param name="a">доданок</param> 
+        /// <param name="b">доданок</param> 
+        /// <returns>сума</returns> 
+        public static int Add(long а, long b) { return (int)(а + b); }
+
+        /// <summary> 
+        /// функція віднімання чисел а і b 
+        /// </summary> 
+        /// <param name="a">зменшуване</param> 
+        /// <param name="b">від’ємне</param> 
+        /// <returns>різниця</returns> 
+        public static int Sub(long а, long b) { return (int)(а - b); }
+
+
+        /// <summary> 
+        /// функція множення чисел а і b 
+        /// </summary> 
+        /// <param name="a">множник</param> 
+        /// <param name="b">множник</param> 
+        /// <returns>добуток</returns> 
+        public static int Mult(long а, long b) { return (int)(а * b); }
+
+        /// <summary> 
+        /// функція знаходження частки 
+        /// </summary> 
+        /// <param name="a">ділене</param> 
+        /// <param name="b">дільник</param> 
+        /// <returns>частка</returns> 
+        public static int Div(long а, long b) { return (int)(а / b); }
+
+        /// <summary> 
+        /// функція ділення по модулю 
+        /// </summary> 
+        /// <param name="a">ділене</param> 
+        /// <param name="b">дільник</param> 
+        /// <returns>остача</returns> 
+        public static long Mod(long а, long b)
+        {
+            ////if (a == b) return 0;
+            ////if (a < b) return 2;
+            return (int)(а % b);
+        }
+
+
+        /// <summary> 
+        /// унарний плюс  
+        /// </summary> 
+        /// <param name="a"></param> 
+        /// <returns></returns> 
+        public static int ABS(long а) { return 1; }
+
+        /// <summary> 
+        /// унарний мінус  
+        /// </summary> 
+        /// <param name="a"></param> 
+        /// <returns></returns> 
+        public static int IABS(long а) { return 1; }
+
+
+        ////Використовується також глобальна змінна: 
+        /// <summary> 
+        /// Останнє повідомлення про помилку. 
+        /// Поле і властивість для нього 
+        /// </summary> 
+        private static string _lastError = "";
+
+        public static string lastError;
+
+
+
+
+
     }
 }
